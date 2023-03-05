402cf0e00800cebae8ae3a835e4c1b5d7a15bbca
yanlele <331393627@qq.com>
2023-03-05 13:05:58 +0800

    feat: 添加枚举

---
+++ utils/Issue/consts.ts
@@ -0,0 +1,6 @@
+export enum MileStone {
+  base = 1, // 初级
+  inProgress, // 中级
+  senior, // 高级别
+  master, // 资深
+}
--- utils/Issue/yanleIssue/writeIssue.ts
+++ utils/Issue/yanleIssue/writeIssue.ts
@@ -2,13 +2,15 @@ import { octokit } from "../main";
 import { apiUrl } from "../../apiUrl";
 import { WriteIssueOptions } from "./interface";
 import * as fs from "fs";
+import { MileStone } from "../consts";
 
 const config = require("../../configToken.json");
 
 const remote = [
   {
-    title: "【中】JS 有哪些迭代器，该如何使用？",
+    title: "什么是同源策略？",
     labels: ["JavaScript"],
+    milestone: MileStone.base,
     body: fs.readFileSync("./demo.md", { encoding: "utf8" }),
   },
 ];
@@ -20,7 +22,7 @@ const write = (options: WriteIssueOptions) => octokit.request(apiUrl.writeIssue,
 
 write(remote[0])
   .then((res: any) => {
-    console.log("yanle - logger: res", res);
+    console.log("yanle - logger: res", res.status);
   })
   .catch((e: Error) => {
     console.log("yanle - logger: e", e);

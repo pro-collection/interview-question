de7ae451101d5cc7252faaf88afbdd1fd7fcd404
yanlele <331393627@qq.com>
2023-03-07 00:12:05 +0800

    feat: 修改 tag

--- utils/Issue/releaseNote/index.ts
+++ utils/Issue/releaseNote/index.ts
@@ -1,28 +1,47 @@
 import { octokit } from "../main";
 import { apiUrl } from "../../apiUrl";
 import repoConfig from "../../repoConfig";
+import { base64ToString, stringToBase64 } from "./helper";
+import { split, toNumber, join, replace } from "lodash";
 
-// package.json 版本自增
+const path = "package.json";
 
 // 获取 package.json
-
-function base64ToString(b64: string) {
-  return Buffer.from(b64, "base64").toString();
-}
-
-function stringToBase64(str: string) {
-  return Buffer.from(str).toString("base64");
-}
-
-
 const getPackageJson = () => octokit.request(apiUrl.getContent, {
   ...repoConfig.interviewRepo,
-  path: "package.json",
+  path,
 });
 
-getPackageJson().then((res: any) => {
-  console.log("yanle - logger: res.data", base64ToString(res.data.content));
+const updatePackageJson = (content: string) => octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+  ...repoConfig.interviewRepo,
+  path,
+  message: "update package version with github api runner",
+  committer: {
+    name: "yanlele",
+    email: "331393627@qq.com",
+  },
+  content: "bXkgbmV3IGZpbGUgY29udGVudHM=",
 });
+
+const main = async () => {
+  // 获取 package.json
+  const res = await getPackageJson();
+  let packageString = base64ToString(res.data.content);
+  const packageJson = JSON.parse(packageString);
+  const currentVersion = Reflect.get(packageJson, "version");
+
+  // package.json 版本自增
+  const [a, b, c] = split(currentVersion, ".");
+  const patch = toNumber(c) + 1;
+  const newVersion = join([a, b, patch]);
+
+  // 替换旧 version
+  packageString = replace(packageString, `"version": "${currentVersion}"`, `"version": "${newVersion}"`);
+
+  // 提交
+  await updatePackageJson(stringToBase64(packageString));
+
+};
 
 
 // 创建 tag
@@ -32,3 +51,5 @@ getPackageJson().then((res: any) => {
 // 整理格式
 
 // 发布 release note
+
+export {};
--- utils/apiUrl.ts
+++ utils/apiUrl.ts
@@ -2,4 +2,5 @@ export const apiUrl = {
   getIssue: "GET /repos/{owner}/{repo}/issues",
   writeIssue: "POST /repos/{owner}/{repo}/issues",
   getContent: "GET /repos/{owner}/{repo}/contents/{path}",
+  updateContent: "PUT /repos/{owner}/{repo}/contents/{path}",
 };

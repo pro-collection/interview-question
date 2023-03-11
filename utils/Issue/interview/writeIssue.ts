a42c38d5eb9b061fc87dcab0d2302c0cf7121101
yanlele <331393627@qq.com>
2023-03-11 22:08:47 +0800

    feat: 更改目录

--- utils/Issue/interview/writeIssue.ts
+++ utils/Issue/interview/writeIssue.ts
@@ -7,9 +7,9 @@ import { octokit } from "../../requestKit";
 
 const remote = [
   {
-    title: "如何做一个前端项目工程的自动化部署， 有哪些规范和流程设计？",
+    title: "纯 TS 项目工程中, 如何使用 alias path?",
     labels: [labels.js],
-    milestone: MileStone.master,
+    milestone: MileStone.senior,
     body: fs.readFileSync('./demo.md', { encoding: "utf8" }),
     // body: fs.readFileSync('/Users/yanle/code/self/node-index/books/专题知识库/02、ECMAScript最新语法/10、Iterator 和 for...of 循环/README.md', { encoding: "utf8" }),
   },

61e803b4b0f38450443b99a934f4f53fc1db004f
yanlele <331393627@qq.com>
2023-03-13 23:19:31 +0800

    feat: gitee api

--- utils/requestKit.ts
+++ utils/requestKit.ts
@@ -1,7 +1,19 @@
-const { Octokit } = require("octokit");
+import { Octokit } from "octokit";
+import axios from "axios";
+import repoConfig from "@utils/repoConfig";
+
 const config = require("./configToken.json");
+
 
 export const octokit = new Octokit({
   auth: config.token,
 });
 
+export const giteeApi = axios.create({
+  params: {
+    access_token: config.gitee_api_token,
+  },
+  data: {
+    access_token: config.gitee_api_token,
+  },
+});

e2624d6017e8245da9a8d89244a94e6ae446c202
yanlele <331393627@qq.com>
2023-03-26 13:41:00 +0800

    feat: 修改 pdf 配置

--- src/mdToPDF/index.ts
+++ src/mdToPDF/index.ts
@@ -3,12 +3,14 @@ import * as path from "path";
 import { mdToPdf } from "md-to-pdf";
 
 const main = async () => {
-  const filePath = path.resolve(__dirname, "../../README.md");
-  const outputPath = path.resolve(__dirname, "../../README.pdf");
+  const filePath = path.resolve(__dirname, "../../books/0.0.16.md");
+  const outputPath = path.resolve(__dirname, "../../books/0.0.16.pdf");
 
   const pdf = await mdToPdf({ path: filePath }, { page_media_type: "print" }).catch(console.error);
 
   if (pdf) fs.writeFileSync(outputPath, pdf.content);
+
+  console.log('yanle - logger: ', filePath);
 };
 
 main();

ac9c4950fb7413dea322f70cfa4e3f85ece34faa
yanlele <331393627@qq.com>
2023-04-17 23:34:08 +0800

    feat: 添加热度搜索

---
+++ src/githubApi/issue/search.ts
@@ -0,0 +1,17 @@
+import { octokit } from "@utils/requestKit";
+import { apiUrl } from "@utils/apiUrl";
+import repoConfig from "@utils/repoConfig";
+import { get } from "lodash";
+import dayjs from "dayjs";
+
+const request = (q: string) => octokit.request(`${apiUrl.searchIssue}?q=${q}`, {
+  ...repoConfig.interviewRepo,
+});
+
+export const search = async (search: string) => {
+  const created = dayjs().subtract(6, "month").format("YYYY-MM-DD");
+  const query = `${search}+created:>${created}&page=1&per_page=1`;
+  const res = await request(query);
+  return get(res, "data.total_count", 0);
+};
+
--- utils/apiUrl.ts
+++ utils/apiUrl.ts
@@ -27,6 +27,9 @@ export const apiUrl = {
   // 更新一个 tag
   // api https://docs.github.com/en/rest/issues/issues?apiVersion=2022-11-28#update-an-issue
   updateTag: "PATCH /repos/{owner}/{repo}/issues/{issue_number}",
+
+  // https://docs.github.com/en/rest/search?apiVersion=2022-11-28#search-issues-and-pull-requests
+  searchIssue: "GET /search/issues",
 };
 
 /**

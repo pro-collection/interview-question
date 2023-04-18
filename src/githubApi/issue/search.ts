897629f4e095e3276b0eaccca4b43a302d4b43a0
yanlele <331393627@qq.com>
2023-04-19 00:07:49 +0800

    feat: 打印搜索接口

--- src/githubApi/issue/search.ts
+++ src/githubApi/issue/search.ts
@@ -3,10 +3,14 @@ import { get } from "lodash";
 import dayjs from "dayjs";
 import axios from "axios";
 
-const request = (q: string, created: string) => axios.request({
-  url: apiUrl.searchIssue(q, created),
-  method: "get",
-});
+const request = (q: string, created: string) => {
+  const url = apiUrl.searchIssue(q, created);
+  console.log("yanle - logger: 获取热度 URL", url);
+  return axios.request({
+    url,
+    method: "get",
+  });
+};
 
 export const search = async (search: string) => {
   const created = dayjs().subtract(1, "year").format("YYYY-MM-DD");

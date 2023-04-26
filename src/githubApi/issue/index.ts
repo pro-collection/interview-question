d2c49aa502ba722b637a03e09514475836df9572
yanlele <331393627@qq.com>
2023-04-26 22:40:54 +0800

    语义化版本 SemVer（Semantic Versioning）了解多少？

--- src/githubApi/issue/index.ts
+++ src/githubApi/issue/index.ts
@@ -6,8 +6,8 @@ import { writeIssue } from "@src/githubApi/issue/writeIssue";
 import { search } from "@src/githubApi/issue/search";
 
 const remote = {
-  title: "npm script 了解多少？",
-  key_world: "npm script 运行",
+  title: "语义化版本 SemVer（Semantic Versioning）了解多少？",
+  key_world: "",
   labels: [
     labels.engineering,
     // company.mi,
@@ -19,17 +19,19 @@ const remote = {
 const main = async () => {
   console.log(`yanle - logger: 使用关键词 【${remote.key_world}】 去获取关键词热度`);
 
-  const count = await search(remote.key_world);
-  remote.title = count && remote.key_world ? `${remote.title}【热度: ${count?.toLocaleString() || count}】` : remote.title;
+  if (remote.key_world) {
+    const count = await search(remote.key_world);
+    remote.title = count && remote.key_world ? `${remote.title}【热度: ${count?.toLocaleString() || count}】` : remote.title;
+  }
 
   // 输出 title
   console.log("yanle - logger: title", remote.title);
 
   // html 写入远端
-  htmlWriteIssue(remote);
+  // htmlWriteIssue(remote);
 
   // md 写入远端
-  // writeIssue(remote);
+  writeIssue(remote);
 
   // 写入本地 temp.md
   // writeToTemp();

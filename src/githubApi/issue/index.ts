faffea05d7fe325a929e7e65e0e16fc929c3fe82
yanlele <331393627@qq.com>
2024-09-17 16:33:02 +0800

    [Vue] vue 中 reactive() 返回的为何是一个原始对象的 Proxy，有和好处【热度: 416】

--- src/githubApi/issue/index.ts
+++ src/githubApi/issue/index.ts
@@ -9,7 +9,7 @@ import { filePath } from "@src/githubApi/file/consts";
 import { input, confirm } from "@inquirer/prompts";
 
 const remote = {
-  title: `[Vue] 深层嵌套的对象、数组或者 JavaScript 内置的数据结构，比如 Map 等， 在响应式使用方面， ref 和 reactive 有何区别吗`,
+  title: `[Vue] vue 中 reactive() 返回的为何是一个原始对象的 Proxy，有和好处`,
   key_world: [
     "响应式 api",
     // "PerformanceObserver PerformanceResourceTiming api 使用",

ea27c8fdf00b9a024fb8b6f47a9c912498c15220
yanlele <331393627@qq.com>
2024-10-05 10:46:29 +0800

    feat: 处理一些 demo

--- src/spider/job_zhipin/index.ts
+++ src/spider/job_zhipin/index.ts
@@ -1,10 +1,12 @@
 // Import puppeteer
 import puppeteer from "puppeteer";
+import * as cheerio from "cheerio";
+import { includes } from "lodash";
 
 (async () => {
   // Launch the browser
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     defaultViewport: {
       width: 1460,
       height: 900,
@@ -14,33 +16,25 @@ import puppeteer from "puppeteer";
   // Create a page
   const page = await browser.newPage();
 
-  //   拦截请求
-  // await page.setRequestInterception(true);
-  // page.on("request", (interceptedRequest) => {
-  //   if (interceptedRequest.isInterceptResolutionHandled()) return;
-
-  //   if (interceptedRequest.url().includes("wapi/zpgeek/search/joblist.json")) {
-  //     console.log(`[yanle] - interceptedRequest`, interceptedRequest);
-  //   }
-
-  //   interceptedRequest.continue();
-  // });
-
-  // await page.setRequestInterception(true);
-  page.on("response", async (response) => {
-    if (response.url().includes("wapi/zpgeek/search/joblist.json")) {
-      const text = await response.text();
-      console.log(`[yanle] - text`, text);
-    }
-  });
-
   // Go to your site
   await page.goto("https://www.zhipin.com/web/geek/job?query=rust&city=101270100");
 
-  // await page.waitForResponse();
-  const element = await page.waitForSelector("body");
+  // 等待这个请求
+  page
+    .waitForResponse(() => true)
+    .then(async (res) => {
+      // console.log(`[yanle] - res url`, res.url());
+      console.log(`[yanle] - res`, await res?.text?.());
+    });
 
-  // console.log(`[yanle] - element`, element);
+  const queryPath =
+    "#wrap > div.page-job-wrapper > div.page-job-inner > div > div.job-list-wrapper > div.search-job-result > ul > li:nth-child(1)";
+  await page.waitForSelector(queryPath);
+
+  const html = await page.content();
+  // console.log(`[yanle] - html`, html);
+
+  // const $ = cheerio.load(html);
 
   // Close browser.
   await browser.close();

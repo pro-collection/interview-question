c1bd058fd46b7369fa36094f0c07dd77dc6903b7
yanlele <331393627@qq.com>
2024-10-15 00:49:44 +0800

    feat: 清洗结果结束

--- temp/juejin_interview/handleData.ts
+++ temp/juejin_interview/handleData.ts
@@ -1,27 +1,39 @@
-import { map, toNumber, trim } from "lodash";
+import { filter, forEach, get, join, map, toNumber, trim } from "lodash";
 // @ts-expect-error
 import jsonData from "./2024_10_01.json";
+import fs from "fs";
 
 /**
  * 修正数据
  */
 const handleData = () => {
-  const data = map(jsonData, (item) => {
-    const applaud = trim(item.applaud)?.replace(/\/n/gi, "");
-
-    const applaudNumber = applaud ? toNumber(applaud) : 0;
-
-    return {
-      ...item,
-      applaud: applaudNumber,
-    };
+  const data = filter(jsonData, (item) => {
+    return item.date?.includes("2024-09");
   });
 
-  const json = JSON.stringify(data);
+  const tagObject: Record<string, string[]> = {};
+  forEach(data, (item) => {
+    const tag = get(item, "tags", "其他");
 
-  const fs = require("fs");
+    const link = `- [${item.name}](${item.url})`;
 
-  fs.writeFile("data.json", json, (err: Error) => {
+    if (tagObject[tag]) {
+      tagObject[tag]?.push(link);
+    } else {
+      tagObject[tag] = [link];
+    }
+  });
+
+  const textString = Object.entries(tagObject)
+    .map(([tag, list]) => {
+      return `### ${tag}
+${join(list, "\n")}`;
+    })
+    .join("\n\n");
+
+  // const json = JSON.stringify(data);
+
+  fs.writeFile("./temp/juejin_interview/2024_10_01.md", textString, (err) => {
     if (err) {
       console.error("Error writing to file:", err);
     } else {

45167a3c8d55951d7d62448d8ce9600949547670
yanlele <331393627@qq.com>
2024-12-15 21:47:48 +0800

    feat: 处理 11 月份的爬虫结果

--- temp/juejin_interview/handleData.ts
+++ temp/juejin_interview/handleData.ts
@@ -1,6 +1,6 @@
 import { filter, first, forEach, get, join, map, orderBy, toNumber, trim } from "lodash";
 // @ts-expect-error
-import jsonData from "./2024_11_02.json";
+import jsonData from "./2024_12_02.json";
 import fs from "fs";
 
 /**
@@ -9,7 +9,7 @@ import fs from "fs";
 const handleData = () => {
   const data = orderBy(
     filter(jsonData, (item) => {
-      return item.date?.includes("2024-09");
+      return item.date?.includes("2024-11");
     }),
     (item) => item.applaud,
     "desc"
@@ -38,7 +38,7 @@ ${join(list, "\n")}`;
 
   // const json = JSON.stringify(data);
 
-  fs.writeFile("./temp/juejin_interview/2024_09_01.md", textString, (err) => {
+  fs.writeFile("./temp/juejin_interview/2024_12_02.md", textString, (err) => {
     if (err) {
       console.error("Error writing to file:", err);
     } else {
